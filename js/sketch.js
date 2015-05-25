11e3a9690725552b078ccc3d8f3201582e037629
gianordoli <gianordoligabriel@yahoo.com.br>
2015-05-25 10:50:09 -0400

    mask bugged :S

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,10 @@
-var img;
-var p5Canvas;
-var pg;
+var p5Canvas;	// canvas
+
+var img;		// original image
+var pg;			// mask
+
+var button;		// apply mask
+var isDrawing;	// toggle mask/drawing display
 
 function preload() {
 	img = loadImage('img/animation_00.png');	
@@ -8,19 +12,30 @@ function preload() {
 
 function setup() {
 	p5Canvas = createCanvas(640, 360);
+
 	pg = createGraphics(640, 360);
 	pg.noStroke();
-	// pg.fill(0);
-	pg.rect(250, 50, 300, 300);
-	img.mask(pg);	
+
+	isDrawing = true;
+
+	button = createButton('Apply Mask');
+	button.mousePressed(applyMask);
 }
 
 function draw() {
 	background(255, 0, 0);
 	image(img, 0, 0);
-	// image(pg, 0, 0);
+
+	if(isDrawing){
+		image(pg, 0, 0, pg.width, pg.height);
+	}
 }
 
 function mouseDragged(){
+	pg.ellipse(mouseX, mouseY, 10, 10);
+}
 
+function applyMask(){
+	img.mask(pg);	
+	isDrawing = false;
 }
